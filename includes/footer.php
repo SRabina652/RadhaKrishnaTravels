278039ad171e85643909634885abeb3611e68ce6
Aashish-46 <aasisjha555@gmail.com>
2023-02-14 11:33:21 +0545

    Inclued navigation and footer separately

---
+++ includes/footer.php
@@ -0,0 +1,62 @@
+<!-------------------- Footer -------------------->
+<div class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="footer-col-1">
+                        <h3>Explore</h3>
+                        <ul>
+                            <li><a href="">Home</a></li>
+                            <li><a href="">Mountain Flight</a></li>
+                            <li><a href="">Trekking</a></li>
+                            <li><a href="">Tour Package</a></li>
+                            <li><a href="">Services</a></li>
+                            <li><a href="">Book Now</a></li>
+                            <li><a href="">About Us</a></li>
+                            <li><a href="">Contact</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-col-2">
+                        <h3>Follow</h3>
+                        <ul>
+                            <li><a href="">Facebook</a></li>
+                            <li><a href="">Instagram</a></li>
+                            <li><a href="">Twitter</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-col-3">
+                        <h3>Legal</h3>
+                        <ul>
+                            <li><a href="">Terms</a></li>
+                            <li><a href="">Privacy</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-col-4">
+                        <h3>Contact</h3>
+                        <ul>
+                            <li>Head Office</li>
+                            <li>Location: Devkota Chowk, Sonauli Road, Bhairahawa, Nepal</li>
+                            <li>Tel No.: O71-525354</li>
+                            <li>Mobile No.: +977-9804454054, +977-9847540061</li>
+                            <li>Email: radhakrishnaholidays@gmail.com</li>
+                        </ul>
+                    </div>
+                </div>
+                <hr>
+                <p class="copyright">Copyright 2023 - Radha Krishna Holidays</p>
+            </div>
+        </div>
+        <hr>
+    </div>
+
+    <!-------------------- JS for Toggle Menu -------------------->
+
+    <!-------------------- JS for Toggle Menu -------------------->
+
+    <!--
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+        -->
+
+</body>
+
+</html>
---
+++ includes/navigation-bar.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Radha Krishna Holidays</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,700;1,600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+-->
+</head>
+
+<body>
+    <div class="background">
+        <div class="navbar">
+            <div class="logo">
+                <h1><a href="">RadhaKrishnaHolidays</a></h1>
+            </div>
+            <nav>
+                <ul id="MenuItems">
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Mountain Flight</a></li>
+                    <li><a href="">Trekking</a></li>
+                    <li><a href="">Tour Package</a></li>
+                    <li><a href="">Services</a></li>
+                    <li><a href="">Book Now</a></li>
+                    <li><a href="">About Us</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+        <hr>
