2b2cdb626e22e13d1f07b09b949655a3aa75bbf8
Aashish Jha <kjaashish19@tbc.edu.np>
2023-02-14 07:19:43 +0545

    Created Home Page - Homehero, Tour destination, Trek destination, Testimonial, Footer

---
+++ home.php
@@ -0,0 +1,349 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Radha Krishna Holidays</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,700;1,600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+-->
+</head>
+
+<body>
+    <div class="background">
+        <div class="navbar">
+            <div class="logo">
+                <h1><a href="">RadhaKrishnaHolidays</a></h1>
+            </div>
+            <nav>
+                <ul id="MenuItems">
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Mountain Flight</a></li>
+                    <li><a href="">Trekking</a></li>
+                    <li><a href="">Tour Package</a></li>
+                    <li><a href="">Services</a></li>
+                    <li><a href="">Book Now</a></li>
+                    <li><a href="">About Us</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+        <hr>
+        <!-------------------- HomeHero -------------------->
+        <div class="homehero">
+            <div class="container">
+                <div class="row">
+                    <div class="col-2">
+                        <div class="textdata">
+                            <h1>Welcome To Radha Krishna Holidays</h1>
+                            <p>Tour operator/agency for Nepal, Tibet and Bhutan in Kathmandu. We provide quality trips for our beloved customers in reasonable cost. As an adventure travel company we offer a wide range of tour and travel activities such as Nepal trekking, hiking, culture and sightseeing tour with professional guides (who can clearly describe the history and religious significant of the places), walking holidays in the low altitude to higher mountain peaks, wildlife tours, rafting, day hiking and many more. Our major operation destinations are Nepal, Tibet, Bhutan and kailash mansarovar yatra.</p>
+                            <a href="" class="btn">Explore Now &#8594;</a>
+                        </div>
+
+                    </div>
+                    <div class="col-2">
+                        <img src="images/homepage/homehero.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <!-------------------- Our Tour Destination -------------------->
+        <div class="tourdestination">
+            <div class="container">
+                <h2 class="titles">Our Tour Destination</h2>
+                <div class="row">
+                    <div class="col-5">
+                        <a href="" class="btn-list">Nepal</a>
+                    </div>
+                    <div class="col-5">
+                        <a href="" class="btn-list">India</a>
+                    </div>
+                    <div class="col-5">
+                        <a href="" class="btn-list">Bhutan</a>
+                    </div>
+                    <div class="col-5">
+                        <a href="" class="btn-list">Tibet</a>
+                    </div>
+                    <div class="col-5">
+                        <a href="" class="btn-list">Outbound</a>
+                    </div>
+                </div>
+                <!-------------------- Our Tour Destination Description slide -------------------->
+
+            </div>
+        </div>
+        <hr>
+        <!-------------------- Our Trekking Destination -------------------->
+        <div class="trekdestination">
+            <div class="container">
+                <h2 class="titles">Our Trekking Destination</h2>
+                <div class="row">
+                    <div class="col-4">
+                        <a href="">
+                            <img src="images/homepage/homehero.jpg" alt="">
+                        </a>
+                        <hr>
+                        <h4>Annapurna</h4>
+                        <p>Annapurna Circuit Trek is a popular classic trek of Nepal. Each year, more than 60,000 trekkers come for this trek from all around the globe. In this trek, you encircle the Annapurna massif and cover between 110-130 miles by walking.</p>
+                    </div>
+                    <div class="col-4">
+                        <a href="">
+                            <img src="images/homepage/homehero.jpg" alt="">
+                        </a>
+                        <hr>
+                        <h4>Dolkha</h4>
+                        <p>Annapurna Circuit Trek is a popular classic trek of Nepal. Each year, more than 60,000 trekkers come for this trek from all around the globe. In this trek, you encircle the Annapurna massif and cover between 110-130 miles by walking.</p>
+                    </div>
+                    <div class="col-4">
+                        <a href="">
+                            <img src="images/homepage/homehero.jpg" alt="">
+                        </a>
+                        <hr>
+                        <h4>Dolpa</h4>
+                        <p>Annapurna Circuit Trek is a popular classic trek of Nepal. Each year, more than 60,000 trekkers come for this trek from all around the globe. In this trek, you encircle the Annapurna massif and cover between 110-130 miles by walking.</p>
+                    </div>
+                    <div class="col-4">
+                        <a href="">
+                            <img src="images/homepage/homehero.jpg" alt="">
+                        </a>
+                        <hr>
+                        <h4>Everest</h4>
+                        <p>Annapurna Circuit Trek is a popular classic trek of Nepal. Each year, more than 60,000 trekkers come for this trek from all around the globe. In this trek, you encircle the Annapurna massif and cover between 110-130 miles by walking.</p>
+                    </div>
+                </div>
+                <p><a href="" class="btn">View All &#8594;</a></p>
+            </div>
+        </div>
+        <hr>
+        <!-------------------- Testimonial -------------------->
+        <div class="testimonial">
+            <div class="container">
+                <h2 class="titles">Testimonial</h2>
+                <div class="row">
+                    <div class="col-3">
+                        <i class="fa fa-quote-left"></i>
+                        <p>Gladly saying, you guys are excellent. When I decided to make a Bhutan trip with you, I made the best choice. Hope to have another trip with you guys.</p>
+                        <div class="rating">
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star-half-o"></i>
+                            <i class="fa fa-star-o"></i>
+                        </div>
+                        <img src="images/homepage/homehero.jpg" alt="">
+                        <h3>Mr. Aashish Jha</h3>
+                    </div>
+                    <div class="col-3">
+                        <i class="fa fa-quote-left"></i>
+                        <p>Gladly saying, you guys are excellent. When I decided to make a Bhutan trip with you, I made the best choice. Hope to have another trip with you guys.</p>
+                        <div class="rating">
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star-half-o"></i>
+                            <i class="fa fa-star-o"></i>
+                        </div>
+                        <img src="images/homepage/homehero.jpg" alt="">
+                        <h3>Mr. Aashish Jha</h3>
+                    </div>
+                    <div class="col-3">
+                        <i class="fa fa-quote-left"></i>
+                        <p>Gladly saying, you guys are excellent. When I decided to make a Bhutan trip with you, I made the best choice. Hope to have another trip with you guys.</p>
+                        <div class="rating">
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star-half-o"></i>
+                            <i class="fa fa-star-o"></i>
+                        </div>
+                        <img src="images/homepage/homehero.jpg" alt="">
+                        <h3>Mr. Aashish Jha</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <!-------------------- Footer -------------------->
+        <div class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="footer-col-1">
+                        <h3>Explore</h3>
+                        <ul>
+                            <li><a href="">Home</a></li>
+                            <li><a href="">Mountain Flight</a></li>
+                            <li><a href="">Trekking</a></li>
+                            <li><a href="">Tour Package</a></li>
+                            <li><a href="">Services</a></li>
+                            <li><a href="">Book Now</a></li>
+                            <li><a href="">About Us</a></li>
+                            <li><a href="">Contact</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-col-2">
+                        <h3>Follow</h3>
+                        <ul>
+                            <li><a href="">Facebook</a></li>
+                            <li><a href="">Instagram</a></li>
+                            <li><a href="">Twitter</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-col-3">
+                        <h3>Legal</h3>
+                        <ul>
+                            <li><a href="">Terms</a></li>
+                            <li><a href="">Privacy</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-col-4">
+                        <h3>Contact</h3>
+                        <ul>
+                            <li>Head Office</li>
+                            <li>Location: Devkota Chowk, Sonauli Road, Bhairahawa, Nepal</li>
+                            <li>Tel No.: O71-525354</li>
+                            <li>Mobile No.: +977-9804454054, +977-9847540061</li>
+                            <li>Email: radhakrishnaholidays@gmail.com</li>
+                        </ul>
+                    </div>
+                </div>
+                <hr>
+                <p class="copyright">Copyright 2023 - Radha Krishna Holidays</p>
+            </div>
+        </div>
+        <hr>
+    </div>
+
+    <!-------------------- JS for Toggle Menu -------------------->
+
+    <!--image slider start-->
+
+    <div class="tourdestination">
+        <div class="container">
+            <div class="tourdestinationdescription">
+                <div class="row">
+                    <div class="slider">
+                        <div class="slides">
+                            <!--radio buttons start-->
+                            <input type="radio" name="radio-btn" id="radio1">
+                            <input type="radio" name="radio-btn" id="radio2">
+                            <input type="radio" name="radio-btn" id="radio3">
+                            <input type="radio" name="radio-btn" id="radio4">
+                            <!--radio buttons end-->
+                            <!--slide images start-->
+                            <div class="slide first">
+                                <div class="tourdestinationdescription">
+                                    <div class="row">
+                                        <div class="col-2">
+                                            <img src="images/homepage/homehero.jpg" alt="">
+                                        </div>
+                                        <div class="col-2">
+                                            <div class="textdata">
+                                                <h1>Nepal Tour</h1>
+                                                <p>Situated in the lap of the mighty Himalayas, the souvenir nation of Nepal is blessed with rich history and bottomless natural beauty. A travellers paradise overfilled with numerous opportunities for sightseeing travel, fascinating geographical marvel, every corner of Nepal tend to mesmerize the foreign visitors with the authentic and varied travel experiences that is purely irresistible as time goes by.</p>
+                                                <a href="" class="btn">Explore Now &#8594;</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="slide">
+                                <div class="tourdestinationdescription">
+                                    <div class="row">
+                                        <div class="col-2">
+                                            <img src="images/homepage/homehero.jpg" alt="">
+                                        </div>
+                                        <div class="col-2">
+                                            <div class="textdata">
+                                                <h1>India Tour</h1>
+                                                <p>Situated in the lap of the mighty Himalayas, the souvenir nation of Nepal is blessed with rich history and bottomless natural beauty. A travellers paradise overfilled with numerous opportunities for sightseeing travel, fascinating geographical marvel, every corner of Nepal tend to mesmerize the foreign visitors with the authentic and varied travel experiences that is purely irresistible as time goes by.</p>
+                                                <a href="" class="btn">Explore Now &#8594;</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="slide">
+                                <div class="tourdestinationdescription">
+                                    <div class="row">
+                                        <div class="col-2">
+                                            <img src="images/homepage/homehero.jpg" alt="">
+                                        </div>
+                                        <div class="col-2">
+                                            <div class="textdata">
+                                                <h1>Bhutan Tour</h1>
+                                                <p>Situated in the lap of the mighty Himalayas, the souvenir nation of Nepal is blessed with rich history and bottomless natural beauty. A travellers paradise overfilled with numerous opportunities for sightseeing travel, fascinating geographical marvel, every corner of Nepal tend to mesmerize the foreign visitors with the authentic and varied travel experiences that is purely irresistible as time goes by.</p>
+                                                <a href="" class="btn">Explore Now &#8594;</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="slide">
+                                <div class="tourdestinationdescription">
+                                    <div class="row">
+                                        <div class="col-2">
+                                            <img src="images/homepage/homehero.jpg" alt="">
+                                        </div>
+                                        <div class="col-2">
+                                            <div class="textdata">
+                                                <h1>Tibet Tour</h1>
+                                                <p>Situated in the lap of the mighty Himalayas, the souvenir nation of Nepal is blessed with rich history and bottomless natural beauty. A travellers paradise overfilled with numerous opportunities for sightseeing travel, fascinating geographical marvel, every corner of Nepal tend to mesmerize the foreign visitors with the authentic and varied travel experiences that is purely irresistible as time goes by.</p>
+                                                <a href="" class="btn">Explore Now &#8594;</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!--slide images end-->
+                            <!--automatic navigation start-->
+                            <div class="navigation-auto">
+                                <div class="auto-btn1"></div>
+                                <div class="auto-btn2"></div>
+                                <div class="auto-btn3"></div>
+                                <div class="auto-btn4"></div>
+                            </div>
+                            <!--automatic navigation end-->
+                        </div>
+                        <!--manual navigation start-->
+                        <div class="navigation-manual">
+                            <label for="radio1" class="manual-btn"></label>
+                            <label for="radio2" class="manual-btn"></label>
+                            <label for="radio3" class="manual-btn"></label>
+                            <label for="radio4" class="manual-btn"></label>
+                        </div>
+                        <!--manual navigation end-->
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <!--image slider end-->
+
+        <script type="text/javascript">
+            var counter = 1;
+            setInterval(function() {
+                document.getElementById('radio' + counter).checked = true;
+                counter++;
+                if (counter > 4) {
+                    counter = 1;
+                }
+            }, 5000);
+        </script>
+    </div>
+    <!-------------------- JS for Toggle Menu -------------------->
+
+    <!--
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+        -->
+
+</body>
+
+</html>
