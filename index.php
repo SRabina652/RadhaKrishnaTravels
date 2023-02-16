d975da3179c5bdc67c6d189b912c870a33f901ca
SRabina652 <srabina19@tbc.edu.np>
2023-02-16 11:58:23 +0545

    link added

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Cover page</title>
 
-  <link rel="stylesheet" href="./assest/styles/index.css">
+  <link rel="stylesheet" href="./assest/styles/index.css?v=1">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
 
 </head>
@@ -25,7 +25,7 @@
         </div>
         <div class="nav-links">
           <ul>
-            <li><a href="#">Home</a></li>
+            <li><a href="home.php">Home</a></li>
             <li><a href="#">About Us</a></li>
             <li><a href="#">Mountain Flight</a></li>
             <li><a href="#">Trekking</a></li>
@@ -35,22 +35,23 @@
         </div>
       </div>
       <!-- middle content of the page -->
-      <div class="bodyContent">
-        <h1>The lovely journey with lovely peoples</h1>
-        <div class="textSlider">
-          <div>
+      <div class="msgContainer">
+        <div class="textContent">
+          <div class="textVal">
+            <h1>The lovely journey with lovely peoples</h1>
+            <p>Tour operator/agency for Nepal, Tibet and Bhutan in Kathmandu. We provide quality trips for our beloved customers in reasonable cost. As an adventure travel company we offer a wide range of tour and travel activities such as Nepal trekking, hiking, culture and sightseeing tour with professional guides (who can clearly describe the history and religious significant of the places), walking holidays in the low altitude to higher mountain peaks, wildlife tours, rafting, day hiking and many more. Our major operation destinations are Nepal, Tibet, Bhutan and kailash mansarovar yatra.</p>
+          </div>
+          <!-- <div class="textVal">
+            <h1>The lovely journey with lovely peoples</h1>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut eos, soluta doloremque iusto quos illum dignissimos nemo sunt libero accusantium accusamus nostrum porro nam inventore quaerat ducimus voluptatum facilis voluptate ipsa quod molestias beatae reiciendis consectetur? Veniam sapiente, in hic perferendis libero adipisci alias qui corrupti, perspiciatis saepe provident sed.</p>
           </div>
-
-          <div>
+          <div class="textVal">
+            <h1>The lovely journey with lovely peoples</h1>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut eos, soluta doloremque iusto quos illum dignissimos nemo sunt libero accusantium accusamus nostrum porro nam inventore quaerat ducimus voluptatum facilis voluptate ipsa quod molestias beatae reiciendis consectetur? Veniam sapiente, in hic perferendis libero adipisci alias qui corrupti, perspiciatis saepe provident sed.</p>
-          </div>
-          <div>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut eos, soluta doloremque iusto quos illum dignissimos nemo sunt libero accusantium accusamus nostrum porro nam inventore quaerat ducimus voluptatum facilis voluptate ipsa quod molestias beatae reiciendis consectetur? Veniam sapiente, in hic perferendis libero adipisci alias qui corrupti, perspiciatis saepe provident sed.</p>
-          </div>
-
+          </div> -->
         </div>
-        <button class="white-btn">Explore</button>
+        
+        <a href="home.php"><button class="white-btn">Explore</button></a>
       </div>
     </div>
   </div>
